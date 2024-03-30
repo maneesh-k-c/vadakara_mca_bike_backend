491ff1364d899771b2c4ea1ddd1457994722d172
maneeshk-chandran <maneesh.maitexa@gmail.com>
2024-03-30 13:21:25 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+app.use(express.static('./public'))
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Database Connected Successfully');
+  })
+  .catch((error) => {
+    console.log('Database Error:', error);
+  });
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('view engine','ejs')
+app.set('views','./src/views')
+
+
+app.get('/', (req, res) => {
+  res.render('login.ejs',{Message: ''})
+});
+
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on ${process.env.PORT}`);
+});
